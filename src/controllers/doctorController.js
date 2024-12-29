89c4cd933cbd56ddbc092dae263b3c94a1f0899e
Tran-Bao-Anh <anh.tranbao.work@gmail.com>
2024-12-29 16:49:46 +0700

    add modle markdown

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -1,13 +1,15 @@
 import doctorService from "../services/doctorService";
 
 let getTopDoctorHome = async (req, res) => {
+    //limit là biến set giới hạn top bác sĩ hiện ra
   let limit = req.query.limit;
   if (!limit) limit = 10;
   try {
-    let response = await doctorService.getTopDoctorHome(+limit);
+    let response = await doctorService.getTopDoctorHome(+limit);    //+limit là chuyển từ limit từ string sang number
     return res.status(200).json(response);
   } catch (e) {
     console.log(e);
+    //status(200) là kết nối thành công
     return res.status(200).json({
       errCode: -1,
       message: "Error from server...",
--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -5,11 +5,11 @@ let getTopDoctorHome = (limitInput) => {
   return new Promise(async (resolve, reject) => {
     try {
       let users = await db.User.findAll({
-        limit: limitInput,
-        where: { roleId: "R2" },
-        order: [["createdAt", "DESC"]],
-        attributes: {
-          exclude: ["password"],
+        limit: limitInput,  // giới hạn kết quả truy vấn
+        where: { roleId: "R2" }, //truy vấn tại các user có roleId: 'R2'
+        order: [["createdAt", "DESC"]], //sắp xếp các user được truy vấn theo ngày tạo "createdAt", lấy thằng vừa tạo lên trước
+        attributes: {   
+          exclude: ["password"],    //bỏ trường password
         },
         include: [
           {
@@ -31,6 +31,7 @@ let getTopDoctorHome = (limitInput) => {
         data: users,
       });
     } catch (e) {
+        //nếu reject thì sẽ chạy ngay vào catch trong hàm getTopDoctorHome trong file doctorController.js
       reject(e);
     }
   });
