d59b8aa83b83a4a53e62cf238e178a4695850eb0
Tran-Bao-Anh <anh.tranbao.work@gmail.com>
2025-02-01 23:32:24 +0700

    #83

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -98,6 +98,19 @@ let getExtraInfoDoctorById = async (req, res) => {
   }
 };
 
+let getProfileDoctorById = async (req, res) => {
+  try {
+    let info = await doctorService.getProfileDoctorById(req.query.doctorId);
+    return res.status(200).json(info);
+  } catch (e) {
+    console.log(e);
+    return res.status(200).json({
+      errCode: -1,
+      errMessage: "Error from the server",
+    });
+  }
+};
+
 module.exports = {
   getTopDoctorHome: getTopDoctorHome,
   getAllDoctors: getAllDoctors,
@@ -106,4 +119,5 @@ module.exports = {
   bulkCreateSchedule: bulkCreateSchedule,
   getScheduleByDate: getScheduleByDate,
   getExtraInfoDoctorById: getExtraInfoDoctorById,
+  getProfileDoctorById: getProfileDoctorById,
 };
