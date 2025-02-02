c90d54bbebfed97225b74c3dda1bea5fb6c37692
Tran-Bao-Anh <anh.tranbao.work@gmail.com>
2025-02-02 12:25:40 +0700

    #84

---
+++ src/services/patientService.js
@@ -0,0 +1,52 @@
+import { where } from "sequelize";
+import db from "../models/index";
+require("dotenv").config();
+
+let postBookAppointment = (data) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      if (!data.email || !data.doctorId || !data.timeType || !data.date) {
+        resolve({
+          errCode: 1,
+          errMessage: "Missing parameter",
+        });
+      } else {
+        //upsert patient
+        let user = await db.User.findOrCreate({
+          where: { email: data.email },
+          defaults: {
+            email: data.email,
+            roleId: "R3",
+          },
+        });
+        console.log(
+          "check user[0] from postBookAppointment in patientService.js: ",
+          user[0]
+        );
+        //create a booking record
+        if (user && user[0]) {
+          await db.Booking.findOrCreate({
+            where: { patientId: user[0].id },
+            defaults: {
+              statusId: "S1",
+              doctorId: data.doctorId,
+              patientId: user[0].id,
+              data: data.date,
+              timeType: data.timeType,
+            },
+          });
+        }
+        resolve({
+          errCode: 0,
+          errMessage: "save info patient succeed!",
+        });
+      }
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+module.exports = {
+  postBookAppointment: postBookAppointment,
+};
